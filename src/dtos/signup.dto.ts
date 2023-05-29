f7839b44122546f4d6966ca2921ec9ecc7cb97ab
Shaikh Abdul Mannan <abdulmannan8334@gmail.com>
2023-05-29 11:58:27 +0530

    new validators

--- src/dtos/signup.dto.ts
+++ src/dtos/signup.dto.ts
@@ -5,21 +5,26 @@ import {
   IsNumber,
   IsObject,
   IsOptional,
+  IsPhoneNumber,
   IsString,
+  Length,
   ValidateNested,
 } from 'class-validator';
 
 export class SignUpname {
   @IsString()
   @IsNotEmpty()
+  @Length(2, 10)
   firstName: string;
 
   @IsString()
   @IsOptional()
+  @Length(2, 10)
   middleName?: string;
 
   @IsString()
   @IsNotEmpty()
+  @Length(2, 10)
   lastName: string;
 }
 
@@ -39,5 +44,6 @@ export class SignUp {
 
   @IsString()
   @IsOptional()
+  @IsPhoneNumber('IN')
   phone?: string;
 }
